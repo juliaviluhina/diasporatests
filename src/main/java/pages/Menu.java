b7473936c6e1b947d9f14a415b7a53ad36205e57
Julia Iluhina <julia.v.iluhina@gmail.com>
2015-10-28 10:54:43 +0200

    28 10 2015 10:54

--- src/main/java/pages/Menu.java
+++ src/main/java/pages/Menu.java
@@ -14,39 +14,27 @@ import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import static java.lang.System.currentTimeMillis;
 import static pages.Aspects.FRIENDS;
 import static pages.Aspects.STANDART_ASPECTS;
 
-public class Menu extends AdditionalAPI{
+public class Menu extends AdditionalAPI {
 
     public static SelenideElement userMenuHeader = $(".user-menu-trigger");//$(".user-menu-more-indicator");
     public static ElementsCollection userMenuItems = $$(".user-menu-item a");
 
     private static String darkHeaderLocator = "header .dark-header";
 
-    //method added because of problem with opening user menu when stream is not loaded
-    private static void openMenu() {
-
-        long startTime = currentTimeMillis();
-        Boolean result = FALSE;
-
-        do {
-            userMenuHeader.click();
-            if (userMenuItems.filter(exactText("Log out")).size() == 1) {
-                    result = TRUE;
-            }
-        } while ((!result) || (startTime + Configuration.timeout < currentTimeMillis()));
-    }
-
     @Step
     public static void logOut() {
-        //userMenuHeader.click();
         openMenu();
-        userMenuItems.find(exactText("Log out")).click();
-        assertLoggedOut();//this check for wait moment when logout will be done - unstable
+        doLogOut();
+//        userMenuItems.find(exactText("Log out")).click();
+//        assertLoggedOut();//this check for wait moment when logout will be done - unstable
+//
     }
 
     @Step
@@ -69,14 +57,14 @@ public class Menu extends AdditionalAPI{
     }
 
     @Step
-    public static void openContacts(){
+    public static void openContacts() {
         //userMenuHeader.click();
         openMenu();
         userMenuItems.find(exactText("Contacts")).click();
     }
 
     @Step
-    public static void ensureLoggedOut(){
+    public static void ensureLoggedOut() {
         //newScreenshot();
         if ($$(darkHeaderLocator).size() != 0) {
             logOut();
@@ -85,11 +73,38 @@ public class Menu extends AdditionalAPI{
     }
 
     @Step
-    public static void assertLoggedOut(){
+    public static void assertLoggedOut() {
         //newScreenshot();
         $$(darkHeaderLocator).shouldBe(empty);
     }
 
+    //method added because of problem with opening user menu when stream is not loaded
+    private static void openMenu() {
+
+        long startTime = currentTimeMillis();
+        Boolean result = FALSE;
+
+        do {
+            userMenuHeader.click();
+            if (userMenuItems.filter(exactText("Log out")).size() == 1) {
+                result = TRUE;
+            }
+        } while ((!result) || (startTime + Configuration.timeout < currentTimeMillis()));
+    }
+
+    //method added because of problem with click on LogOut when stream is not loaded
+    private static void doLogOut() {
+
+        long startTime = currentTimeMillis();
+        Boolean result = FALSE;
+
+        do {
+            userMenuItems.find(exactText("Log out")).click();
+            if ($$(darkHeaderLocator).size() == 0) {
+                result = TRUE;
+            }
+        } while ((!result) || (startTime + Configuration.timeout < currentTimeMillis()));
+    }
 
 
 }
