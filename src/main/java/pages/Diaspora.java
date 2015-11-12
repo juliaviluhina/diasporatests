68e9562d473c63f2775bc7ec6d52ed1deaa522ae
Julia Iluhina <julia.v.iluhina@gmail.com>
2015-11-12 17:56:11 +0200

    12 11 2015 17:56

--- src/main/java/pages/Diaspora.java
+++ src/main/java/pages/Diaspora.java
@@ -3,22 +3,53 @@ package pages;
 import com.codeborne.selenide.SelenideElement;
 import datastructures.PodUser;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import ru.yandex.qatools.allure.annotations.Step;
 
 import static com.codeborne.selenide.Condition.present;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
+import static core.AdditionalAPI.assertThat;
+import static core.AdditionalAPI.elementExceptionsCatcher;
+import static core.AdditionalAPI.timeout2x;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
 public class Diaspora {
 
     @Step
     public static void signInAs(PodUser user) {
-        open(user.podLink + "/users/sign_in");
+        //open(user.podLink + "/users/sign_in");
+        assertThat(authenticationIsOpened(user), timeout2x());
         userName.setValue(user.userName);
         $("#user_password").setValue(user.password);
         $(By.name("commit")).click();
     }
 
     public static SelenideElement userName = $("#user_username");
+
+    private static ExpectedCondition<Boolean> authenticationIsOpened(final PodUser user) {
+        return elementExceptionsCatcher(new ExpectedCondition<Boolean>() {
+
+            public Boolean apply(WebDriver webDriver) {
+                open(user.podLink + "/users/sign_in");
+                if (!userName.is(visible)) {
+                    Menu.logOut();
+                    return FALSE;
+                }
+                return TRUE;
+            }
+
+            @Override
+            public String toString() {
+                return "Error opening authentication";
+            }
+
+        });
+    }
 
 }
