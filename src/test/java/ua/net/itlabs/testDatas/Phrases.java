ca9990d1bccf06031c6ed67b014125935d99e82a
Julia Iluhina <julia.v.iluhina@gmail.com>
2015-12-10 11:17:25 +0200

    10 12 2015 11:17

--- src/test/java/ua/net/itlabs/testDatas/Phrases.java
+++ src/test/java/ua/net/itlabs/testDatas/Phrases.java
@@ -2,9 +2,9 @@ package ua.net.itlabs.testDatas;
 
 public class Phrases {
 
-    public static final String PUBLIC_POST = "Public post";
-    public static final String POST_FOR_FRIENDS = "Post for friends";
-    public static final String MENTION_POST = "Mention post";
-    public static final String COMMENT = "Comment";
+    public static final String PUBLIC_POST = "Public information";
+    public static final String POST_FOR_FRIENDS = "Information for friends";
+    public static final String MENTION_POST = "Mention information";
+    public static final String COMMENT = "Answer in comment";
 
 }
